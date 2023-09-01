682eca7841fe7ad76b61ef78e9728bece492111c
Onur Ozer <onurozer@gmail.com>
2023-09-02 07:49:23 +0800

    added udpate method to check tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[update destroy]
+
   def index
     @tasks = Task.all
   end
@@ -7,12 +9,21 @@ class TasksController < ApplicationController
   end
 
   def update
+    if @task.update(task_params)
+      redirect_to tasks_url, notice: "Task was successfully updated."
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 
   def destroy
   end
 
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :checked)
