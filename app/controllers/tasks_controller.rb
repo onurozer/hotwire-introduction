0ab165d522fb5ede976dc59fc4d57bf1b54b618f
Onur Ozer <onurozer@gmail.com>
2023-09-02 06:58:58 +0800

    added tasks controller and routes

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,13 @@
+class TasksController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :tasks, only: [:index, :create, :update, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # resource
 end
