ba33d2bf1362117ff90de6818ff6a7b680fb0cf1
Onur Ozer <onurozer@gmail.com>
2023-09-02 11:14:44 +0800

    added progress bar for turbo

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import "@hotwired/turbo-rails";
+import "./controllers";
+
+// Displays a progress bar during turbo navigation
+// Turbo.setProgressBarDelay(1);
