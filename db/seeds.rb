8d93fa9473bdb0e3c315f124d03ddba8626e0b46
Onur Ozer <onurozer@gmail.com>
2023-09-02 08:22:54 +0800

    destroy everything before seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+Task.destroy_all
 Task.create([
   {title: "Buy groceries"},
   {title: "Wash the car"},
