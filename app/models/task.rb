5af664afd4ff18aa3e4a4aaa09033776ce937840
Onur Ozer <onurozer@gmail.com>
2023-09-02 14:37:36 +0800

    broadcast task updates

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,13 @@
 class Task < ApplicationRecord
   validates :title, presence: true
+
+  after_create_commit -> {
+    broadcast_append_to "tasks_activity", target: "tasks", partial: "tasks/task"
+  }
+  after_update_commit -> {
+    broadcast_replace_to "tasks_activity", target: self, partial: "tasks/task"
+  }
+  after_destroy_commit -> {
+    broadcast_remove_to "tasks_activity"
+  }
 end
