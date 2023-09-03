9d7ef59972b6e128aafbf6b8912bacc82ced9a3d
Onur Ozer <onurozer@gmail.com>
2023-09-03 09:23:06 +0800

    added turbo disable option

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,5 +2,8 @@
 import "@hotwired/turbo-rails";
 import "./controllers";
 
+// Disable Turbo
+// Turbo.session.drive = false;
+
 // Displays a progress bar during turbo navigation
 // Turbo.setProgressBarDelay(1);
